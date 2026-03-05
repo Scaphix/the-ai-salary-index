c78db6bf29ce23c381b5d48a5d44036a124964ff
Scaphix <chahinez.hoelzenbein@gmail.com>
2026-03-05 22:09:50 +0100

    Update app.py file for deployement on Heroku

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-from app_pages.custom_transformers import OrdinalMappingEncoder, FrequencyEncoder  # noqa: F401
+from src.machine_learning.custom_transformers import OrdinalMappingEncoder, FrequencyEncoder  # noqa: F401
 
 from app_pages.page_summary import page_summary_body
 from app_pages.page_salary_study import page_salary_study_body
 from app_pages.page_predict_salary import page_predict_salary_body
 from app_pages.page_cluster import page_cluster_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
 st.set_page_config(
     page_title="The AI Salary Index",
@@ -13,6 +14,7 @@ st.set_page_config(
 
 pages = st.navigation([
     st.Page(page_summary_body, title="Quick Project Summary"),
+    st.Page(page_project_hypothesis_body, title="Project Hypothesis"),
     st.Page(page_salary_study_body, title="AI Salary Study"),
     st.Page(page_predict_salary_body, title="Predict Salary"),
     st.Page(page_cluster_body, title="Job Market Clusters"),
