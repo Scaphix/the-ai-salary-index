d143ea913df35d5b8dae2c162a8d52da3f0fcd7d
Scaphix <chahinez.hoelzenbein@gmail.com>
2026-03-11 08:59:10 +0100

    Fix: Add the missing import of the cluster page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_salary_study import page_salary_study_body
 from app_pages.page_predict_salary import page_predict_salary_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_cluster import page_cluster_body
 
 st.set_page_config(
     page_title="TASI",
@@ -28,7 +29,7 @@ pages = st.navigation([
     st.Page(page_salary_study_body, title="AI Salary Study"),
     st.Page(page_project_hypothesis_body, title="Project Hypothesis"),
     st.Page(page_predict_salary_body, title="Predict Salary"),
-    st.page(page_cluster, title="Cluster Analysis"),
+    st.Page(page_cluster_body, title="Cluster Analysis"),
 ])
 
 pages.run()
